5f53c0a9a55d93fe689bdac3c80ed2df88b75404
John Osezele <johnosezele@gmail.com>
2024-07-21 01:34:55 +0100

    helper files added

--- helper-files/address_field.dart
+++ helper-files/address_field.dart
@@ -15,3 +15,57 @@ Future<List<AddressInfo>> _onAddressSearch(String address) async {
     }
     return _places;
   }
+
+//buildPlaces list widget
+Widget _buildPlacesList() {
+    if (!_showResults) {
+      return const Text("No results found");
+    }
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topRight,
+          child: InkWell(
+            child: const Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+            onTap: () {
+              _places.clear();
+              setState(() {});
+            },
+          ),
+        ),
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: _places.length,
+          itemBuilder: (context, index) {
+            final place = _places[index];
+            return Column(
+              children: [
+                ListTile(
+                  title: Text(place.displayName),
+                  subtitle: Text(
+                      '${place.houseAddress}, ${place.state}, ${place.country}'),
+                  onTap: () {
+                    final addressInfo = AddressInfo(
+                        houseAddress: place.houseAddress,
+                        state: place.state,
+                        country: place.country,
+                        displayName: place.displayName);
+                    _handleSelectedPlace(context, addressInfo);
+                  },
+                  dense: true,
+                  contentPadding: EdgeInsets.zero,
+                  minLeadingWidth: 0,
+                  leading: const SizedBox(),
+                  minVerticalPadding: 0,
+                ),
+                const Divider(),
+              ],
+            );
+          },
+        )
+      ],
+    );
+  }
