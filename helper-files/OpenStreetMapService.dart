1eac8ca9d2214b74b6e6ea1befe22ee1ab44d2dd
John Osezele <johnosezele@gmail.com>
2024-08-05 12:06:58 +0100

    helper files added

--- helper-files/OpenStreetMapService.dart
+++ helper-files/OpenStreetMapService.dart
@@ -2,3 +2,11 @@
 
 //for debugging purpose
 print("API response: ${response.body}");
+
+
+final houseAddressValues = [
+  addressInfo['house_number'],
+  addressInfo['house_name'],
+  addressInfo['road'],
+  addressInfo['place'],
+];
